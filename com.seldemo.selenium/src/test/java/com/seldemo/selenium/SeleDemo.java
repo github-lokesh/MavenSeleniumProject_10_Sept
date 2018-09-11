570ea5bb78100853edb8114da83724af0fd13bf1
User1_Eclipse <user.email@yopmail.com>
2018-09-11 20:37:58 +0530

    Added simple working project

--- com.seldemo.selenium/src/test/java/com/seldemo/selenium/SeleDemo.java
+++ com.seldemo.selenium/src/test/java/com/seldemo/selenium/SeleDemo.java
@@ -1,6 +1,32 @@
 package com.seldemo.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class SeleDemo {
 	//just a random comment : 1 
-	//just a random comment : 2 
+	//just a random comment : 2
+	//Maven Build life cycle
+	/*Clean - preclean, clean, postclean
+	 *Default - validate, compile, test, package, verify, install, deploy
+	 *Site -
+	 *
+	 *
+	 * On cmd prompt
+	 * -------------
+	 * mvn clean test
+	 * mvn clean install
+	 * mvn clean install site
+	 * mvn surefire:test - only executes the tests on previously complied code and no build phases.
+	 * 
+	 */
+	@Test
+	public void f()
+	{
+		System.setProperty("webdriver.chrome.driver","C://Data_Program//Selenium_Dependencies//chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+	}
 }
